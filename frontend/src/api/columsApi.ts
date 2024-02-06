9b38d01e0946f68d5c34b8f7e6f30cf306191349
bassel-new <128236840+bassel-new@users.noreply.github.com>
2024-02-06 20:51:08 +0100

    Create columsApi.ts

---
+++ frontend/src/api/columsApi.ts
@@ -0,0 +1,10 @@
+import { getData } from "./_baseApi";
+import { BaseResponse } from "./_baseApi";
+import { ColumnModul } from "../utility/columnModul";
+
+
+
+
+export async function getCoumnsByProjectId(porjectId:string|undefined):Promise<BaseResponse<ColumnModul>> {
+    return await getData({apiUrl:`/api/column/projectId=${porjectId}`})
+}
